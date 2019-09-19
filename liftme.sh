1dea5d71c6f0dd0aa4d56ed8e597264ef8fc99bc
Yarix <admin@yarix.su>
2019-09-19 11:56:32 +0300

    make some docker moves it liftme.sh deploy script

--- liftme.sh
+++ liftme.sh
@@ -3,5 +3,17 @@
 if [ ! -f .env ] 
 then 
 	echo "No .env File in current folder. You can't deploy without credentials"
+else
+	echo "Importing ENV from .env file"
 fi
+
+mkdir testapp && cd testapp
+git clone https://github.com/soar/devops-test.git
+
+cp ../../docker/docker-compose.yml .
+cp ../../docker/devops-test/Dockerfile devops-test/
+
+docker-compose build .
+
+
 
